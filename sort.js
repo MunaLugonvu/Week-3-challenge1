8763dee890fd156cd61a72395d3e062bf5e14366
MunaLugonvu <munalugonvu@gmail.com>
2019-12-09 19:13:42 +0300

    update

--- sort.js
+++ sort.js
@@ -3,20 +3,20 @@
 var evens = [];
 var odds = [];
 var chars = [];
-let sortedObj = { 'evens': [], 'odds': [], 'chars': [] };
+let sortedObj = { 'evens': [], 'odds':[], 'chars':[] };
 function sortArray(arr1) {
   for (const el of arr1) {
     if (typeof (el) == "number") {
       if (el % 2 == 0) {
-        sortedObj.evens.push(el);
-       
+        sortedObj.evens.sort((a,b) => a - b).push(el);
+
       } else {
 
-        sortedObj.odds.push(el);
+        sortedObj.odds.sort((a,b) => a - b).push(el);
       }
 
     } else if (typeof (el) == "string") {
-      sortedObj.chars.push(el)
+      sortedObj.chars.sort().push(el)
 
 
     }
@@ -24,7 +24,7 @@ function sortArray(arr1) {
   };
   return sortedObj
 }
-console.log(sortArray(['a', 'b', 5, 3, 9, 'i', 7,8,12,14]))
+console.log(sortArray(['a', 'g', 5, 3,'d', 9, 'b', 7, 8, 14, 12,4,2,17,1]))
 
 
 
