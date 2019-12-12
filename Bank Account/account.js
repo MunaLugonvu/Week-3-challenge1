673147ebe2ac7b695804c88b5bdde330355ef4a5
MunaLugonvu <munalugonvu@gmail.com>
2019-12-12 18:00:23 +0300

    update

--- Bank Account/account.js
+++ Bank Account/account.js
@@ -1,26 +1,27 @@
-class BankAccount{
+class BankAccount {
 
 
-    constructor(state,bal=0,AccountName){
+    constructor(state, bal, AccountName) {
         //your code here
-this.state = state;
-this.bal= bal;
-this.AccountName = AccountName;
+        this.state = state;
+        this.bal = bal;
+        this.AccountName = AccountName;
     }
 
-    checkBalance(){
+    checkBalance() {
         //your code here
-        if(this.state == "Opened"){
-    console.log("Your balance is " +this.bal );
+        if (this.state == "Opened") {
+            console.log("Your balance is " + this.bal);
+            return this.bal;
         } else {
             return 'This account is either closed or doesnot exist';
         }
 
     }
 
-    openAccount(){
+    openAccount() {
         //your code here
-        if(this.state == null){
+        if (this.state == null) {
             this.state = "Opened";
             console.log("Your Account is now open!");
             return "Account Opened"
@@ -31,44 +32,49 @@ this.AccountName = AccountName;
 
     }
 
-    deposit( depositAmount){
+    deposit(depositAmount) {
         //your code here
-       if(typeof depositAmount !== "number"){
-           return 'Please enter a Valid Amount';
-       } else{
-           if(this.state !== "Opened"){
-               return  "Invalid Account";
-           }else{
-               this.bal = this.bal + depositAmount;
-              return 'You have deposited'+ depositAmount + "Your balance is" + this.bal;
-           }
-       }
-    }
-    
-    withdraw(withdrawAmount){
-        //Your Code here
-        if(this.state == "Opened"){
-            if(this.bal<withdrawAmount){
-                console.log('Your Account Balance is Insufficient!')
-            }else if(this.bal>withdrawAmount){
-                this.bal = this.bal - withdrawAmount;
-                console.log('You have withdrawn' + withdrawAmount + 'Your new  balance is' + this.bal);
-
-            }else {
-                this.bal = this.bal - withdrawAmount;
-                console.log('All your money has been withdrawn! Your account balance is'+this.bal );
+        if (typeof depositAmount !== "number") {
+            return 'Please enter a valid amount';
+        } else {
+            if (this.state !== "Opened") {
+                return "Invalid Account";
+            } else {
+                this.bal = this.bal + depositAmount;
+                console.log('You have deposited' + depositAmount + "Your balance is" + this.bal);
+                return this.bal;
             }
-        }else {
-            console.log('Withdraw can not be made, Account is closed');
         }
     }
 
-    closeAccount( ){
+    withdraw(withdrawAmount) {
+        //Your Code here
+        if (this.state == "Opened") {
+            if (this.bal < withdrawAmount) {
+                console.log('Your Account Balance is Insufficient!')
+                return "Your Account Balance Is Insufficient!";
+            } else if (this.bal > withdrawAmount) {
+                this.bal = this.bal - withdrawAmount;
+                console.log('You have withdrawn' + withdrawAmount + 'Your new  balance is' + this.bal);
+                return this.bal;
+            } else {
+                this.bal = this.bal - withdrawAmount;
+                console.log('All your money has been withdrawn! Your account balance is' + this.bal);
+                return this.bal;
+            }
+        } else {
+            console.log('Withdraw can not be made, Account is closed');
+            return this.state;
+        }
+    }
+
+    closeAccount() {
         this.state = "Closed"
         console.log('Your Account has been Closed!');
-
+        return 'Account has Been Closed!'
     }
 }
 
- module.exports = new BankAccount(null,500000,'muna');
+module.exports = new BankAccount(null, 500000, "Muna Lugonvu");
+console.log(new BankAccount (null, 500000, "Muna Lugonvu"))
 
--- Bank Account/account.test.js
+++ Bank Account/account.test.js
@@ -5,10 +5,27 @@ test('returns Your Account is now open! if state is Opened', () =>{
 
 });
 
-test ('returns balance',() => {
+test ('returns balance if your account is open',() => {
     expect(BankAccount.checkBalance()).toBe(500000);
 });
 
-test('returns balance after withdraw', () => {
-     expect(BankAccount.withdraw(100000)).toBe(400000);
+test('returns balance after deposit if account state is Opened', () => {
+     expect(BankAccount.deposit(100000)).toBe(600000);
 });
+test('String as Amount is not Accepted', () => {
+    expect( BankAccount.deposit("A hundred Thousand")).toBe("Please enter a valid amount")
+})
+
+test('returns All your money has been withdrawn', () => {
+    expect(BankAccount.withdraw(600000)).toBe(0);
+});
+
+test('returns account  balance is insufficinet if amount is more than balance and account is open', () => {
+    expect(BankAccount.withdraw(1500000)).toBe(" Your Account Balance Is Insufficient!");
+});
+
+
+test('returns your account has been closed',() => {
+    expect(BankAccount.closeAccount()).toBe('Account has Been Closed!');
+});
+
